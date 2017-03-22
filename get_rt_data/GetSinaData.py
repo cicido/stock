1c65e02a49b6a8cfbd5755a3ee2d1310fda3f4c5
duanxiping <cicido@126.com>
2017-03-22 09:59:28 +0800

    增加实时获取sina股票接口代码

--- get_rt_data/GetSinaData.py
+++ get_rt_data/GetSinaData.py
@@ -18,23 +18,24 @@ if __name__ == '__main__':
         data = urllib2.urlopen("http://hq.sinajs.cn/list=%s%s" %(prefix,id)).read().decode('cp936')
 
     data = data[data.find('"')+1:data.rfind('"')].split(",")
-    print data
+    #print data
 
     dataArr = data[:30] +[ data[30]+" " +data[31]]
-    print dataArr
+    #print dataArr
     colsName = ['s_name', 's_open','s_last_close','s_current',
                 's_high','s_low','s_buy_1','s_sell_1','s_count',
                 's_money']
-    buyCols = [['s_buy%d_count' %i,'s_buy%d' %i] for i in range(1,6) ]
+    buyCols = [['s_buy%d_count' %i,'s_buy%d' %i] for i in range(1,6)]
     flattenBuyCols = [ y for x in buyCols for y in x]
     sellCols = [['s_sell%d_count' % i, 's_sell%d' % i] for i in range(1, 6)]
     flattenSellCols = [y for x in sellCols for y in x]
-    colsName = colsName + flattenBuyCols + flattenSellCols + ['datetime']
-    print colsName
-    print len(colsName)
-    print len(dataArr)
-    for i in range(len(dataArr)):
-        print colsName[i] + ":" + dataArr[i]
+    #colsName = colsName + flattenBuyCols + flattenSellCols + ['datetime']
+    colsName = colsName
+    #print colsName
+    #print len(colsName)
+    #print len(dataArr)
+    for i in range(len(colsName)):
+        print colsName[i] + ":  " + dataArr[i]
 
 
 
